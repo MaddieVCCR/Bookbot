6343d5cf89c537e28aee7f23a3a5b5e82e2fb8c8
MaddieVCCR <Maddison.verboom@vccr.nl>
2025-06-05 10:24:13 +0200

    added the option to choose the name of the book

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
