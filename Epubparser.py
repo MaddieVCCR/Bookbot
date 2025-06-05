7978a9f4618c5643a0af9b3e864cf0ff61d750bc
MaddieVCCR <Maddison.verboom@vccr.nl>
2025-06-05 15:53:51 +0200

    epub now parses

---
+++ Epubparser.py
@@ -0,0 +1,18 @@
+import zipfile
+import sys
+from html.parser import HTMLParser
+import re
+from collections import Counter
+
+def main():
+    epub_path = sys.argv[1]
+    try:
+        with zipfile.ZipFile(epub_path, "r") as epub:
+            files = epub.namelist()
+            print(f"contents of {epub_path}:")
+            for file in files:
+                print(file)
+    except Exception as e:
+        print(f"error: {e}")
+
+main()
